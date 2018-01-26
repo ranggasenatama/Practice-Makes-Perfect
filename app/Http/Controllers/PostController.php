f72b86cb98207e4ef36ef401ed12d0752d5ef641
Rangga <ranggasenatama@gmail.com>
2018-01-26 20:18:58 +0700

    Setting up laravel collective

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -111,7 +111,7 @@ class PostController extends Controller
     {
         $post = Post::findOrFail($id);
 
-        $post->delete();
+        $post->forceDelete();
 
         return redirect('/posts');
     }
--- resources/views/post/create.blade.php
+++ resources/views/post/create.blade.php
@@ -1,15 +1,34 @@
 @extends('layouts.app')
 
 @section('content')
+    {{--  <form action="/posts" method="post">  --}}
+        {{--  <input type="text" name="title" placeholder="Title" required>
+        {{csrf_field()}}
+        <input type="submit" name"submit">  --}}
 
     <h1>CREATE FORM</h1>
 
-    <form action="/posts" method="post">
-        <input type="text" name="title" placeholder="Title" required>
-        {{csrf_field()}}
-        <input type="submit" name"submit">
+    {!! Form::open(['method'=>'POST','action'=>'PostController@store']) !!}
 
-    </form>
+    <div class="form-group">
+
+        {!! Form::label('title','Title',['class'=>'kelas anu']) !!}
+        {!! Form::text('title',null,['class'=>'form-control','placeholder'=>'Title']) !!}
+
+    </div>
+    <div class="form-group">
+
+        {!! Form::label('content','Content',['class'=>'kelas anu']) !!}
+        {!! Form::textarea('content',null,['class'=>'form-control','placeholder'=>'Isi Post']) !!}
+    
+    </div>
+    <div class="form-group">
+
+        {!! Form::submit('Save',['class'=>'btn']) !!}
+
+    </div>
+
+    {!! Form::close() !!}
 @endsection
 
 @section('footer')
